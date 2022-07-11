87ca42bee8c763bc31a624f9986d8a5c94feb296
Riccardo <lupinu.ric@gmail.com>
2022-07-11 15:29:08 +0200

    Project: Use Append Method

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-const spesa = ['Pane', 'Formaggio', 'Insalata', 'Farina', 'Verdure', 'Pasta'];
+const spesa = ['Pane', 'Formaggio', 'Insalata', 'Farina', 'Verdure', 'Pasta', 'Sale'];
 let x = 0;
 let listaDom = document.getElementById('lista');
 while (x < spesa.length) {
-    listaDom.innerHTML += `<li>${spesa[x]}</li>`;
-    x++;
+    //InnerHTML
+
+    // listaDom.innerHTML += `<li>${spesa[x]}</li>`;
+    // x++;
+
+    //Append 
+
+    let liDom = document.createElement("li");
+    listaDom.append(liDom);
+    liDom.append(spesa[x]);
+    x++
 }
